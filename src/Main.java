2df5ff8fa1a0fdb10df4525a9940e8b7962e4b32
Patricknunes24 <patrickwandersonnunes@gmail.com>
2023-11-09 10:30:15 -0300

    wworkingWithFile

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* // LENDO ARQUIVOS DE TEXTO COM A CLASSE FILE
+
+        File file = new File("C:\\Users\\patri\\Desktop\\CURSO DE JAVA UDEMY\\Trabalhando_Com_Arquivos\\arquivos\\in.txt");
+        file.canExecute();
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println(" there was an exception "+e.getMessage());
+        }
+        finally {
+            if (sc!= null){
+                sc.close();
+            }
+        }
+         */
+//-----------------------------------------------------------------------------------------
+        String path = "C:\\\\Users\\\\patri\\\\Desktop\\\\CURSO DE JAVA UDEMY\\\\Trabalhando_Com_Arquivos\\\\arquivos\\\\in.txt";
+
+
+
+    }
+}
